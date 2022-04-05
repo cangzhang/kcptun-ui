3f0528f28b8c1b83b79e69e6023fcccd0692e5e2
al <cangzan@gmail.com>
2022-04-06 00:01:04 +0800

    change: check binary.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,28 +1,212 @@
+use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
 use std::env;
 
-fn check_bin() {
-    // let path = Path::new("./")
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LatestReleaseResponse {
+    pub url: String,
+    #[serde(rename = "assets_url")]
+    pub assets_url: String,
+    #[serde(rename = "upload_url")]
+    pub upload_url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    pub id: i64,
+    pub author: Author,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "tag_name")]
+    pub tag_name: String,
+    #[serde(rename = "target_commitish")]
+    pub target_commitish: String,
+    pub name: String,
+    pub draft: bool,
+    pub prerelease: bool,
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+    #[serde(rename = "published_at")]
+    pub published_at: String,
+    pub assets: Vec<Asset>,
+    #[serde(rename = "tarball_url")]
+    pub tarball_url: String,
+    #[serde(rename = "zipball_url")]
+    pub zipball_url: String,
+    pub body: String,
+    pub reactions: Reactions,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Author {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Asset {
+    pub url: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    pub name: String,
+    pub uploader: Uploader,
+    #[serde(rename = "content_type")]
+    pub content_type: String,
+    pub state: String,
+    pub size: i64,
+    #[serde(rename = "download_count")]
+    pub download_count: i64,
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+    #[serde(rename = "updated_at")]
+    pub updated_at: String,
+    #[serde(rename = "browser_download_url")]
+    pub browser_download_url: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Uploader {
+    pub login: String,
+    pub id: i64,
+    #[serde(rename = "node_id")]
+    pub node_id: String,
+    #[serde(rename = "avatar_url")]
+    pub avatar_url: String,
+    #[serde(rename = "gravatar_id")]
+    pub gravatar_id: String,
+    pub url: String,
+    #[serde(rename = "html_url")]
+    pub html_url: String,
+    #[serde(rename = "followers_url")]
+    pub followers_url: String,
+    #[serde(rename = "following_url")]
+    pub following_url: String,
+    #[serde(rename = "gists_url")]
+    pub gists_url: String,
+    #[serde(rename = "starred_url")]
+    pub starred_url: String,
+    #[serde(rename = "subscriptions_url")]
+    pub subscriptions_url: String,
+    #[serde(rename = "organizations_url")]
+    pub organizations_url: String,
+    #[serde(rename = "repos_url")]
+    pub repos_url: String,
+    #[serde(rename = "events_url")]
+    pub events_url: String,
+    #[serde(rename = "received_events_url")]
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    #[serde(rename = "site_admin")]
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Reactions {
+    pub url: String,
+    #[serde(rename = "total_count")]
+    pub total_count: i64,
+    #[serde(rename = "+1")]
+    pub n1: i64,
+    #[serde(rename = "-1")]
+    pub n12: i64,
+    pub laugh: i64,
+    pub hooray: i64,
+    pub confused: i64,
+    pub heart: i64,
+    pub rocket: i64,
+    pub eyes: i64,
+}
+
+async fn check_bin() -> Result<String> {
     let os = match env::consts::OS {
         "windows" => "windows",
         "linux" => "linux",
         "macos" => "darwin",
-        _ => ""
+        _ => "",
     };
     let arch = match env::consts::ARCH {
         "x86" => "386",
         "x86_64" => "amd64",
         _ => "",
     };
+
+    if os.chars().count() == 0 || arch.chars().count() == 0 {
+        return Err(anyhow!("Cannot determine `OS`/`ARCH`"));
+    }
+
+    let bin_name = format!("client_{}_amd64.exe", os);
+    let bin_path = std::env::current_dir().unwrap().join("bin").join(bin_name);
+    println!("{}", bin_path.to_string_lossy());
+    if bin_path.exists() {
+        return Ok(String::from(bin_path.to_string_lossy()));
+    }
+
     let tag = format!("{}-{}", os, arch);
-    println!("tag is: {}", tag)
+    let resp = reqwest::get("https://api.github.com/repos/xtaci/kcptun/releases/latest")
+        .await?
+        .json::<LatestReleaseResponse>()
+        .await?;
+    let mut url = String::from("");
+    for asset in resp.assets.iter() {
+        if asset.name.contains(&tag) {
+            url = asset.browser_download_url.clone();
+            break;
+        }
+    }
+    if url.chars().count() == 0 {
+        return Err(anyhow!("cannot find bin for {} from latest release", tag));
+    }
+
+    println!("got target, {}", url);
+
+    Ok(url)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_check_bin() {
-        check_bin()
+    #[tokio::test]
+    async fn test_check_bin() {
+        match check_bin().await {
+            Ok(_) => println!("check ok"),
+            Err(e) => println!("{:?}", e),
+        }
     }
 }
