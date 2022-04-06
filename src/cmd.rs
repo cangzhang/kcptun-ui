5a253d8b678510d7c426aeeac2eb7fe06b02630a
al <cangzan@gmail.com>
2022-04-06 23:07:00 +0800

    feat: download & untar binary.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,159 +1,53 @@
 use anyhow::{anyhow, Result};
+use flate2::read::GzDecoder;
 use serde::{Deserialize, Serialize};
-use std::env;
+use std::{
+    env,
+    fs::{self, File},
+    io::{self, Cursor},
+    path::Path,
+};
+use tar::Archive;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct LatestReleaseResponse {
     pub url: String,
-    #[serde(rename = "assets_url")]
     pub assets_url: String,
-    #[serde(rename = "upload_url")]
     pub upload_url: String,
-    #[serde(rename = "html_url")]
     pub html_url: String,
     pub id: i64,
-    pub author: Author,
-    #[serde(rename = "node_id")]
     pub node_id: String,
-    #[serde(rename = "tag_name")]
     pub tag_name: String,
-    #[serde(rename = "target_commitish")]
     pub target_commitish: String,
     pub name: String,
     pub draft: bool,
     pub prerelease: bool,
-    #[serde(rename = "created_at")]
     pub created_at: String,
-    #[serde(rename = "published_at")]
     pub published_at: String,
     pub assets: Vec<Asset>,
-    #[serde(rename = "tarball_url")]
     pub tarball_url: String,
-    #[serde(rename = "zipball_url")]
     pub zipball_url: String,
-    pub body: String,
-    pub reactions: Reactions,
+    // pub body: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Author {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Asset {
     pub url: String,
     pub id: i64,
-    #[serde(rename = "node_id")]
     pub node_id: String,
     pub name: String,
-    pub uploader: Uploader,
-    #[serde(rename = "content_type")]
     pub content_type: String,
     pub state: String,
     pub size: i64,
-    #[serde(rename = "download_count")]
     pub download_count: i64,
-    #[serde(rename = "created_at")]
     pub created_at: String,
-    #[serde(rename = "updated_at")]
     pub updated_at: String,
-    #[serde(rename = "browser_download_url")]
     pub browser_download_url: String,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Uploader {
-    pub login: String,
-    pub id: i64,
-    #[serde(rename = "node_id")]
-    pub node_id: String,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "gravatar_id")]
-    pub gravatar_id: String,
-    pub url: String,
-    #[serde(rename = "html_url")]
-    pub html_url: String,
-    #[serde(rename = "followers_url")]
-    pub followers_url: String,
-    #[serde(rename = "following_url")]
-    pub following_url: String,
-    #[serde(rename = "gists_url")]
-    pub gists_url: String,
-    #[serde(rename = "starred_url")]
-    pub starred_url: String,
-    #[serde(rename = "subscriptions_url")]
-    pub subscriptions_url: String,
-    #[serde(rename = "organizations_url")]
-    pub organizations_url: String,
-    #[serde(rename = "repos_url")]
-    pub repos_url: String,
-    #[serde(rename = "events_url")]
-    pub events_url: String,
-    #[serde(rename = "received_events_url")]
-    pub received_events_url: String,
-    #[serde(rename = "type")]
-    pub type_field: String,
-    #[serde(rename = "site_admin")]
-    pub site_admin: bool,
-}
+static APP_USER_AGENT: &str = "KCPTUN_UI v1";
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Reactions {
-    pub url: String,
-    #[serde(rename = "total_count")]
-    pub total_count: i64,
-    #[serde(rename = "+1")]
-    pub n1: i64,
-    #[serde(rename = "-1")]
-    pub n12: i64,
-    pub laugh: i64,
-    pub hooray: i64,
-    pub confused: i64,
-    pub heart: i64,
-    pub rocket: i64,
-    pub eyes: i64,
-}
-
-async fn check_bin() -> Result<String> {
+async fn check_bin_and_download() -> Result<String> {
     let os = match env::consts::OS {
         "windows" => "windows",
         "linux" => "linux",
@@ -178,24 +72,41 @@ async fn check_bin() -> Result<String> {
     }
 
     let tag = format!("{}-{}", os, arch);
-    let resp = reqwest::get("https://api.github.com/repos/xtaci/kcptun/releases/latest")
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+    let json = client
+        .get("https://api.github.com/repos/xtaci/kcptun/releases/latest")
+        .send()
         .await?
         .json::<LatestReleaseResponse>()
         .await?;
-    let mut url = String::from("");
-    for asset in resp.assets.iter() {
-        if asset.name.contains(&tag) {
-            url = asset.browser_download_url.clone();
+
+    let mut asset = &Asset::default();
+    for a in json.assets.iter() {
+        if a.name.contains(&tag) {
+            asset = &a;
             break;
         }
     }
-    if url.chars().count() == 0 {
+    let download_url = &asset.browser_download_url;
+    if download_url.chars().count() == 0 {
         return Err(anyhow!("cannot find bin for {} from latest release", tag));
     }
 
-    println!("got target, {}", url);
+    let resp = client.get(download_url).send().await?;
+    let mut gz_file = Cursor::new(resp.bytes().await?);
 
-    Ok(url)
+    let tar_path = Path::new(".").join("bin").join(&asset.name);
+    fs::create_dir_all(tar_path.parent().unwrap())?;
+    let mut download_tar = File::create(&tar_path)?;
+    io::copy(&mut gz_file, &mut download_tar)?;
+
+    let tar_gz = File::open(tar_path)?;
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    archive.unpack("./bin")?;
+    Ok(String::from(bin_path.to_string_lossy()))
 }
 
 #[cfg(test)]
@@ -203,9 +114,9 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn test_check_bin() {
-        match check_bin().await {
-            Ok(_) => println!("check ok"),
+    async fn test_bin() {
+        match check_bin_and_download().await {
+            Ok(p) => println!("== check ok ==, {}", p),
             Err(e) => println!("{:?}", e),
         }
     }
